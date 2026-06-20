028a23ae9c64cbee87b915df5b4cadf1c3980067
Ttruc-NguyenNgoc <trutruc12122007@gmail.com>
2026-06-20 14:09:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import streamlit as st
 
 # Tiêu đề ứng dụng
+st.image("IMG_20260522_095123.jpg", width=200)
+
 st.title("💵 Ứng dụng tính Thuế Thu nhập cá nhân")
+st.subheader("Nguyễn Ngọc Thanh Trúc")
 
 # Nhập dữ liệu
 thu_nhap = st.number_input(
@@ -16,23 +19,20 @@ nguoi_phu_thuoc = st.number_input(
     value=0
 )
 
+# Nút tính toán
 if st.button("Tính thuế"):
 
-    # ===== TÍNH BẢO HIỂM =====
-    bhxh = thu_nhap * 0.08
-    bhyt = thu_nhap * 0.015
-    bhtn = thu_nhap * 0.01
+    # Bảo hiểm bắt buộc
+    bao_hiem = thu_nhap * 0.105
 
-    tong_bao_hiem = bhxh + bhyt + bhtn
-
-    # ===== GIẢM TRỪ GIA CẢNH =====
+    # Giảm trừ gia cảnh
     giam_tru_ban_than = 15.5
     giam_tru_phu_thuoc = 6.2 * nguoi_phu_thuoc
 
-    # ===== THU NHẬP TÍNH THUẾ =====
+    # Thu nhập tính thuế
     thu_nhap_tinh_thue = (
         thu_nhap
-        - tong_bao_hiem
+        - bao_hiem
         - giam_tru_ban_than
         - giam_tru_phu_thuoc
     )
@@ -40,7 +40,7 @@ if st.button("Tính thuế"):
     if thu_nhap_tinh_thue < 0:
         thu_nhap_tinh_thue = 0
 
-    # ===== TÍNH THUẾ TNCN =====
+    # Tính thuế lũy tiến từng phần
     if thu_nhap_tinh_thue <= 10:
         thue = thu_nhap_tinh_thue * 0.05
 
@@ -74,18 +74,20 @@ if st.button("Tính thuế"):
             + (thu_nhap_tinh_thue - 100) * 0.35
         )
 
-    # ===== LƯƠNG THỰC NHẬN =====
-    luong_net = thu_nhap - tong_bao_hiem - thue
+    # Lương thực nhận
+    luong_net = thu_nhap - bao_hiem - thue
 
-    # ===== KẾT QUẢ =====
-    st.success("Kết quả tính toán")
+    # Hiển thị kết quả
+    st.success("KẾT QUẢ TÍNH TOÁN")
 
-    st.write(f"📌 BHXH (8%): {bhxh:.2f} triệu đồng")
-    st.write(f"📌 BHYT (1,5%): {bhyt:.2f} triệu đồng")
-    st.write(f"📌 BHTN (1%): {bhtn:.2f} triệu đồng")
-    st.write(f"📌 Tổng bảo hiểm (10,5%): {tong_bao_hiem:.2f} triệu đồng")
+    st.write(
+        f"📌 Bảo hiểm bắt buộc (10,5%): {bao_hiem:.2f} triệu đồng"
+    )
 
-    st.write(f"📌 Giảm trừ bản thân: {giam_tru_ban_than:.2f} triệu đồng")
+    st.write(
+        f"📌 Giảm trừ bản thân: {giam_tru_ban_than:.2f} triệu đồng"
+    )
+
     st.write(
         f"📌 Giảm trừ người phụ thuộc: {giam_tru_phu_thuoc:.2f} triệu đồng"
     )
